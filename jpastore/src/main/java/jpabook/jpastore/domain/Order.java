2c1389a30f0f591cd45b92ec0370d0b532b9821f
DongHyun Jo <mr8356@konkuk.ac.kr>
2023-02-02 09:36:50 +0000

    Add Linking Mirror Method && Modify persisting Cascade.ALL

--- jpastore/src/main/java/jpabook/jpastore/domain/Order.java
+++ jpastore/src/main/java/jpabook/jpastore/domain/Order.java
@@ -20,10 +20,12 @@ public class Order {
     @JoinColumn(name = "member_id") //FK = Member테이블의 member_id
     private Member member;
 
-    @OneToMany(mappedBy = "order")
+    // 하위 엔티티에도 persist 명령 전달 => Cascade.ALL 
+    // order를 저장할때 아이템과 딜리버리는 자동저장해야되므로 -> cascade
+    @OneToMany(mappedBy = "order", cascade = CascadeType.ALL)
     private List<OrderItem> orderItems = new ArrayList<>();
 
-    @OneToOne
+    @OneToOne(cascade = CascadeType.ALL)
     @JoinColumn(name = "deilvery_id")
     private Delivery delivery;
 
@@ -31,4 +33,22 @@ public class Order {
 
     @Enumerated(EnumType.STRING)
     private OrderStatus status; // 주문 상태 [ORDER, CANCLE]
+
+    //== 연관관계 편이 양방향 메소드 ==//
+
+    // 보통 order 추가할때 member.getOrders().add(order); order.setMember(member); 하지만,
+    // 아래로 오버로딩해주면 order.setMember(order) 하면 끝남. === 보통 member가 order를 추가하므로.
+    public void setMember(Member member){
+        this.member = member;
+        member.getOrders().add(this);
+    }
+    public void addOrderItem(OrderItem orderItem) {
+        this.orderItems.add(orderItem);
+        orderItem.setOrder(this);
+    }
+    public void setDelivery(Delivery delivery) {
+        this.delivery = delivery;
+        delivery.setOrder(this);
+    }
+    //=== 없어도 되지만 있으면 더 편함 ===//
 }
