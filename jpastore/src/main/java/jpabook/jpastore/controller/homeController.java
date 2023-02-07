b2ba1e6ab2672470ff93a8a2afe4e69ec86bf54e
DongHyun Jo <mr8356@konkuk.ac.kr>
2023-02-07 14:49:18 +0000

    home

---
+++ jpastore/src/main/java/jpabook/jpastore/controller/homeController.java
@@ -0,0 +1,17 @@
+package jpabook.jpastore.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Controller
+@Slf4j
+public class homeController {
+    @GetMapping("/")
+    public String home() {
+        log.info("spring home");
+        return "home";
+    }
+    
+}
